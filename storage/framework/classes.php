e830dd8fce19a6ac70af11429c7720e20505c577
Phangs <wlpineda@gmail.com>
2020-10-02 13:32:04 +0800

    Initialize

---
+++ storage/framework/classes.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins/october/demo/Plugin.php',
+  'backend\\Controllers\\index' => 'modules/backend/controllers/index.php',
+  'backend\\Controllers\\auth' => 'modules/backend/controllers/auth.php',
+  'system\\Controllers\\settings' => 'modules/system/controllers/settings.php',
+);
